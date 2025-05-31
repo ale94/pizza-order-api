377657e84ba6606e3534dde66020c23f6919df75
ale94 <alejandrorua111@gmail.com>
2025-05-31 19:31:53 -0300

    feat: add customer and order relationship

--- src/main/java/com/ale94/pizza_order_api/domain/entities/CustomerEntity.java
+++ src/main/java/com/ale94/pizza_order_api/domain/entities/CustomerEntity.java
@@ -1,23 +1,29 @@
 package com.ale94.pizza_order_api.domain.entities;
 
 import java.io.Serializable;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity(name = "customer")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class CustomerEntity implements Serializable{
+public class CustomerEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,5 +38,15 @@ public class CustomerEntity implements Serializable{
     private String phone;
     @Column(length = 30)
     private String email;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @OneToMany(
+        cascade = CascadeType.ALL, 
+        fetch = FetchType.EAGER, 
+        orphanRemoval = true, 
+        mappedBy = "customer"
+    )
+    private List<OrderEntity> orders;
 
 }
--- src/main/java/com/ale94/pizza_order_api/domain/entities/OrderEntity.java
+++ src/main/java/com/ale94/pizza_order_api/domain/entities/OrderEntity.java
@@ -10,9 +10,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,7 +27,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class OrderEntity implements Serializable {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -45,5 +48,9 @@ public class OrderEntity implements Serializable {
 
     @Column(length = 50)
     private String observations;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id")
+    private CustomerEntity customer;
 
 }
