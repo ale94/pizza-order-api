e3994120c331b74a8ca6e955b05c5e70c35bd9d3
ale94 <alejandrorua111@gmail.com>
2025-06-02 22:11:58 -0300

    fix: add item order request

--- src/main/java/com/ale94/pizza_order_api/api/models/requests/OrderRequest.java
+++ src/main/java/com/ale94/pizza_order_api/api/models/requests/OrderRequest.java
@@ -1,6 +1,7 @@
 package com.ale94.pizza_order_api.api.models.requests;
 
 import java.io.Serializable;
+import java.util.List;
 
 import com.ale94.pizza_order_api.util.PaymentMethod;
 
@@ -16,8 +17,7 @@ import lombok.NoArgsConstructor;
 public class OrderRequest implements Serializable {
 
     private String idCustomer;
-    private String idPizza;
     private PaymentMethod paymentMethod;
     private String observations;
-
+    private List<ItemOrder> items;
 }
