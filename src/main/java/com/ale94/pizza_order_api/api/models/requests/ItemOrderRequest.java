e5db3bd07f51ef6c6e181ba4e4b5139ff7afa169
ale94 <alejandrorua111@gmail.com>
2025-06-05 00:11:48 -0300

    feat: add item order request

---
+++ src/main/java/com/ale94/pizza_order_api/api/models/requests/ItemOrderRequest.java
@@ -0,0 +1,17 @@
+package com.ale94.pizza_order_api.api.models.requests;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ItemOrderRequest implements Serializable {
+    private Long idPizza;
+    private Integer quantity;
+}
