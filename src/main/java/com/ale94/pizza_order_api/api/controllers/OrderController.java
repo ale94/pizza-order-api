c7dd1dc9d690221de903432700460da08dcffa33
ale94 <alejandrorua111@gmail.com>
2025-06-08 10:21:05 -0300

    feat: add order controller

--- src/main/java/com/ale94/pizza_order_api/api/controllers/OrderController.java
+++ src/main/java/com/ale94/pizza_order_api/api/controllers/OrderController.java
@@ -2,7 +2,11 @@ package com.ale94.pizza_order_api.api.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,9 +24,24 @@ public class OrderController {
 
     private final IOrderService orderService;
 
+    @GetMapping(path = "{id}")
+    public ResponseEntity<OrderResponse> get(@PathVariable Long id) {
+        return ResponseEntity.ok(this.orderService.read(id));
+    }
+
     @PostMapping
     public ResponseEntity<OrderResponse> post(@RequestBody OrderRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(this.orderService.create(request));
     }
 
+    @PutMapping(path = "{id}")
+    public ResponseEntity<OrderResponse> update(@RequestBody OrderRequest request, @PathVariable Long id) {
+        return ResponseEntity.ok(this.orderService.update(request, id));
+    }
+
+    @DeleteMapping(path = "{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        this.orderService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
